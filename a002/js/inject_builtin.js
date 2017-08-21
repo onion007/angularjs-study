0b7619ae59dd37105e7e84f5e09c56e84e7fab2f
何聪华 <hech@hotdoor.cn>
2017-08-21 10:58:43 +0800

    add a002

---
+++ a002/js/inject_builtin.js
@@ -0,0 +1,5 @@
+var myMod = angular.module('myApp', []);
+myMod.controller('controllerA', ['$scope', '$window', function($scope, $window) {
+    $scope.message = "My Module Has Loaded!";
+    $window.alert($scope.message);
+}]);
